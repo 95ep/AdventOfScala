f54e716d288c1fa788c0c6d424ef50439b833844
95ep <erik.ao.persson@gmail.com>
2023-01-04 17:04:37 +0100

    Day 01, 2022 solution

---
+++ src/main/scala/y2022/Day01.scala
@@ -0,0 +1,34 @@
+package y2022
+package day01
+
+class Day01(input_path: String):
+  var cal_per_elf: List[Int] = List()
+  summarize_calories()
+  val sorted_cal_per_elf: List[Int] = cal_per_elf.sorted
+
+  private def summarize_calories(): Unit =
+    val src = scala.io.Source.fromFile(input_path)
+    try
+      val lines = src.getLines()
+      var current_elf: Int = 0
+      var next_food_item: String = ""
+      while lines.hasNext do
+        next_food_item = lines.next()
+        if next_food_item == "" then
+          // append current_elf calories to list
+          cal_per_elf = current_elf :: cal_per_elf
+          current_elf = 0
+        else current_elf = current_elf + next_food_item.toInt
+
+      cal_per_elf = current_elf :: cal_per_elf
+    finally src.close
+    // append last elf
+
+  def solution1: Unit =
+    println(s"Solution to part 1 is: ${sorted_cal_per_elf.last}")
+
+  def solution2: Unit =
+    val top_three_elves = sorted_cal_per_elf.takeRight(3)
+    var cumulative_calories = 0
+    top_three_elves.foreach(x => cumulative_calories += x)
+    println(s"Solution to part 2 is: $cumulative_calories")
