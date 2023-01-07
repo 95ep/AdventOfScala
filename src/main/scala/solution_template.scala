05906f560f5c3d897498b86d96f1730130107245
95ep <erik.ao.persson@gmail.com>
2023-01-07 16:40:55 +0100

    Add solution template

---
+++ src/main/scala/solution_template.scala
@@ -0,0 +1,6 @@
+package y202x
+
+package dayXX
+
+class DayXX(input_path: String):
+  println("Solution template")
--- src/main/scala/y2022/Main.scala
+++ src/main/scala/y2022/Main.scala
@@ -1,6 +1,11 @@
 package y2022
 
-@main def hello: Unit =
+@main def main(day: Int): Unit =
+  day match
+    case 1 => run_day01
+    case _ => println(s"No solution for day ${day} implemented!")
+
+def run_day01: Unit =
   val d1 = y2022.day01.Day01("inputs/y2022/day01.txt")
   d1.solution1
   d1.solution2
