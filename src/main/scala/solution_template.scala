2e3479b7844f455dced9f18946ebe5c1d124b77a
95ep <erik.ao.persson@gmail.com>
2023-01-07 16:43:19 +0100

    Update solution template

--- src/main/scala/solution_template.scala
+++ src/main/scala/solution_template.scala
@@ -3,4 +3,8 @@ package y202x
 package dayXX
 
 class DayXX(input_path: String):
-  println("Solution template")
+  def solution1: Unit =
+    println(s"Solution to part 1 is: ${}")
+
+  def solution2: Unit =
+    println(s"Solution to part 2 is: ${}")
