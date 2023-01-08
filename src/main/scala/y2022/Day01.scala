ad117409b9303209c17d144fc30d067c0efa2302
95ep <erik.ao.persson@gmail.com>
2023-01-08 16:04:21 +0100

    Fix comment

--- src/main/scala/y2022/Day01.scala
+++ src/main/scala/y2022/Day01.scala
@@ -20,9 +20,9 @@ class Day01(input_path: String):
           current_elf = 0
         else current_elf = current_elf + next_food_item.toInt
 
+      // Append last elf
       cal_per_elf = current_elf :: cal_per_elf
     finally src.close
-    // append last elf
 
   def solution1: Unit =
     println(s"Solution to part 1 is: ${sorted_cal_per_elf.last}")
