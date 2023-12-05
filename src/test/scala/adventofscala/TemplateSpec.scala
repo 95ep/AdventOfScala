1bc7ee6df53848d5de49ba772f5e49022cf18a14
95ep <erik.ao.persson@gmail.com>
2023-12-05 08:26:56 +0100

    Update spec template - again

--- src/test/scala/adventofscala/TemplateSpec.scala
+++ src/test/scala/adventofscala/TemplateSpec.scala
@@ -7,23 +7,23 @@ import org.scalatest.matchers.should.Matchers
 
 class DayXXSpec extends AnyFlatSpec with Matchers {
 
-  val dayXX = new DayXX
+  val day = new DayXX
 
   behavior of "part 1"
   it should "pass the example test" in {
-    dayXX.part1("test-inputs/y20xx/dayXX.txt") should be(1)
+    day.part1("test-inputs/y20xx/dayXX.txt") should be(1)
   }
 
   it should "solve for real input" in {
-    dayXX.part1("solution-inputs/y20xx/dayXX.txt")
+    day.part1("solution-inputs/y20xx/dayXX.txt")
   }
 
   behavior of "part 2"
   it should "pass the example test" in {
-    dayXX.part2("test-inputs/y20xx/dayXX.txt") should be(1)
+    day.part2("test-inputs/y20xx/dayXX.txt") should be(1)
   }
 
   it should "solve for real input" in {
-    dayXX.part2("solution-inputs/y20xx/dayXX.txt")
+    day.part2("solution-inputs/y20xx/dayXX.txt")
   }
 }
