67b9c828f3c00fd4c6a845e4b943d938f25c8ebe
95ep <erik.ao.persson@gmail.com>
2023-12-07 09:02:11 +0100

    fix tempalte

--- src/test/scala/adventofscala/TemplateSpec.scala
+++ src/test/scala/adventofscala/TemplateSpec.scala
@@ -17,15 +17,15 @@ class DayXXSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "solve for real input" in {
-    solution.part1("solution-inputs/y$year/day$day.txt")
+    solution.part1(s"solution-inputs/y$year/day$day.txt")
   }
 
   behavior of "part 2"
   it should "pass the example test" in {
-    solution.part2("test-inputs/y$year/day$day.txt") should be(1)
+    solution.part2(s"test-inputs/y$year/day$day.txt") should be(1)
   }
 
   it should "solve for real input" in {
-    solution.part2("solution-inputs/y$year/day$day.txt")
+    solution.part2(s"solution-inputs/y$year/day$day.txt")
   }
 }
