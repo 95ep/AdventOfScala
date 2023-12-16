46e65c43fa6a327f1abafe054ddc91709e6d4a69
95ep <erik.ao.persson@gmail.com>
2023-12-16 15:32:47 +0100

    Day 12 part 2 solution

--- src/main/scala/adventofscala/y2023/Day12.scala
+++ src/main/scala/adventofscala/y2023/Day12.scala
@@ -1,57 +1,92 @@
 package adventofscala.y2023
 import adventofscala.utils.FileLoader
+import scala.annotation.tailrec
 
 class Day12 extends FileLoader {
 
-  def parseLine(line: String): (String, List[Int], List[Int]) = {
-    val springs = (line.split(" ")(0))
+  def parseLine(line: String, repeats: Int): (String, List[Int], List[Int]) = {
+    val springs = ("?" + line.split(" ")(0)).repeat(repeats).drop(1)
     val idxUnknown = springs.zipWithIndex
       .filter((spring, _) => spring == '?')
       .map((_, idx) => idx)
       .toList
 
-    val groups = line.split(" ")(1).split(',').map(_.toInt).toList
+    val groups =
+      Range(0, repeats)
+        .flatMap(_ => line.split(" ")(1).split(',').map(_.toInt))
+        .toList
     (springs, idxUnknown, groups)
   }
 
-  def isValid(
-      arrangement: List[Int],
-      row: String,
-      groups: List[Int]
+  def validPlacement(
+      springs: String,
+      startIdx: Int,
+      groupSize: Int
   ): Boolean = {
-    val updatedRow = arrangement.foldLeft(row)((s, i) => s.updated(i, '#'))
-    val groupSizes = """#+""".r.findAllIn(updatedRow).map(_.size).toList
-    groupSizes == groups
+    val endIdx = startIdx + groupSize
+    if (endIdx > springs.size) false
+    else if (endIdx == springs.size) {
+      val r = s"""[#?]{$groupSize}""".r
+      r.matches(springs.substring(startIdx, endIdx))
+    } else {
+      val r = s"""[#?]{$groupSize}[.?]""".r
+      r.matches(springs.substring(startIdx, endIdx + 1))
+    }
   }
 
-  def solution(inputList: List[String]): Int = {
+  @tailrec
+  private def possibleArrangements(
+      springs: String,
+      groups: List[Int],
+      prevPlacement: Map[Int, Long]
+  ): Long = {
+    if (groups.isEmpty)
+      prevPlacement
+        .filter((k, _) => springs.drop(k).indexOf('#') == -1)
+        .foldLeft(0L)(_ + _._2)
+    else {
+      val newPrevPlacement = prevPlacement
+        .map((prev, count) => {
+          val lastStart =
+            if (springs.drop(prev).indexOf("#") != -1)
+              springs.drop(prev).indexOf("#") + (if (prev != -1) prev else 0)
+            else springs.size - 1
+          Range(prev + 1, lastStart + 1)
+            .filter(i => validPlacement(springs, i, groups.head))
+            .map(i => (i + groups.head, count))
+            .groupMapReduce(_._1)(_._2)((a, b) => a + b)
+        })
+        .reduce((m1, m2) =>
+          m1 ++ m2.map { case (k, v) => k -> (v + m1.getOrElse(k, 0L)) }
+        )
+
+      possibleArrangements(springs, groups.tail, newPrevPlacement)
+    }
+  }
+
+  def solution2(inputList: List[String], repeats: Int): Long = {
+    println("Solution 2")
     inputList
       .map(line => {
-        val parsedLine = parseLine(line)
-        val damagedToPlace = parsedLine._3.sum - parsedLine._1.count(_ == '#')
-        val possibleArrangements = parsedLine._2.combinations(damagedToPlace)
-        val validArrangements = possibleArrangements.filter(arrangement =>
-          isValid(arrangement, parsedLine._1, parsedLine._3)
-        )
-        validArrangements.size
+        val parsedLine = parseLine(line, repeats)
+        possibleArrangements(parsedLine._1, parsedLine._3, Map(-1 -> 1L))
       })
       .sum
   }
 
-  def part1(inputPath: String): Int = {
+  def part1(inputPath: String): Long = {
     println("Running part 1")
     val inputList: List[String] = loadLines(inputPath).toList
-    val answer = solution(inputList)
+    val answer = solution2(inputList, 1)
 
     println(s"${this.getClass()}: The answer to part one is $answer")
     answer
   }
 
-  def part2(inputPath: String): Int = {
+  def part2(inputPath: String): Long = {
     println("Running part 2")
     val inputList: List[String] = loadLines(inputPath).toList
-    // val answer = solution(inputList, 5)
-    val answer = 1
+    val answer = solution2(inputList, 5)
 
     println(s"${this.getClass()}: The answer to part two is $answer")
     answer
